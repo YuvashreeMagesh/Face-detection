15a775922990c0febc3f3d995251ec1f3bb6ea0a
Yuvashree M <128991477+YuvashreeMagesh@users.noreply.github.com>
2024-04-29 12:05:56 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,20 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Enter a number: ', (number) => {
+  // Convert input string to a number
+  number = parseInt(number);
+
+  // Check if the number is even or odd
+  if (number % 2 === 0) {
+    console.log(`${number} is even.`);
+  } else {
+    console.log(`${number} is odd.`);
+  }
+
+  rl.close();
+});
